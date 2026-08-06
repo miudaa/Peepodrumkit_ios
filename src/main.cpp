21ce546cec3277b6f1236dd235197b30c46b7e9a
miudaa <miudaa@users.noreply.github.com>
2026-08-06 13:47:38 +0900

    Fix: define SDL_MAIN_HANDLED in main.cpp to prevent _SDL_main linker error

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
+#define SDL_MAIN_HANDLED
 #include "peepodrumkit/chart_editor_main.h"
 #include "core/core_io.h"
-
-#if defined(__APPLE__) || defined(SDL_PLATFORM_IOS)
-#include <SDL3/SDL_main.h>
-#endif
 
 #if _WIN32
 
